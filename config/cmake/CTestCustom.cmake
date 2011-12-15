94f6cb87682cb6727e1315c2dab7f0d16bc39c0a
Allen Byrne <byrn@hdfgroup.org>
2011-12-15 12:08:16 -0600

    Add initial file commit - CMake configuration and scripts. Autotools files need to be added. The source files are from the hdf4 source repo and the folder structure is similiar - fortran subdir examples has been promoted to the fortran folder. JIRA HDFFR-1277

---
+++ config/cmake/CTestCustom.cmake
@@ -0,0 +1,17 @@
+SET (CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS 1500)
+
+SET (CTEST_CUSTOM_WARNING_EXCEPTION
+    ${CTEST_CUSTOM_WARNING_EXCEPTION}
+    "SZIP.src.*:[ \t]*warning"
+    "jpeg.src.*:[ \t]*warning"
+    "POSIX name for this item is deprecated"
+    "disabling jobserver mode"
+)
+ 
+ 
+SET (CTEST_CUSTOM_MEMCHECK_IGNORE
+    ${CTEST_CUSTOM_MEMCHECK_IGNORE}
+    HDF_FORTRAN_EXAMPLES-clearall-objects
+    MFHDF_EXAMPLES-clearall-objects
+    MFHDF_TEST-clearall-objects
+)
---
+++ config/cmake/cacheinit.cmake
@@ -0,0 +1,13 @@
+# This is the CMakeCache file.
+
+########################
+# EXTERNAL cache entries
+########################
+
+SET (USE_SHARED_LIBS OFF CACHE BOOL "Use Shared Libraries" FORCE)
+
+SET (HDF_BUILD_FORTRAN ON CACHE BOOL "Build FORTRAN support" FORCE)
+
+SET (BUILD_TESTING ON CACHE BOOL "Build HDF5 Unit Testing" FORCE)
+
+SET (HDF_ENABLE_PARALLEL OFF CACHE BOOL "Enable parallel build (requires MPI)" FORCE)
