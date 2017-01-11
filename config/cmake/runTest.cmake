563bfdaf9ecf4a8ff1605286a373d5697497a7e3
Allen Byrne <byrn@hdfgroup.org>
2017-01-11 15:28:42 -0600

    Update CMake command conventions.

--- config/cmake/runTest.cmake
+++ config/cmake/runTest.cmake
@@ -5,31 +5,39 @@ cmake_policy(SET CMP0007 NEW)
 # arguments checking
 if (NOT TEST_PROGRAM)
   message (FATAL_ERROR "Require TEST_PROGRAM to be defined")
-endif (NOT TEST_PROGRAM)
+endif ()
 #if (NOT TEST_ARGS)
 #  message (STATUS "Require TEST_ARGS to be defined")
-#endif (NOT TEST_ARGS)
+#endif ()
 if (NOT TEST_FOLDER)
   message ( FATAL_ERROR "Require TEST_FOLDER to be defined")
-endif (NOT TEST_FOLDER)
+endif ()
 if (NOT TEST_OUTPUT)
   message (FATAL_ERROR "Require TEST_OUTPUT to be defined")
-endif (NOT TEST_OUTPUT)
+endif ()
 if (NOT TEST_EXPECT)
   message (STATUS "Require TEST_EXPECT to be defined")
-endif (NOT TEST_EXPECT)
+endif ()
 #if (NOT TEST_FILTER)
 #  message (STATUS "Require TEST_FILTER to be defined")
-#endif (NOT TEST_FILTER)
+#endif ()
 if (NOT TEST_SKIP_COMPARE AND NOT TEST_REFERENCE)
   message (FATAL_ERROR "Require TEST_REFERENCE to be defined")
-endif (NOT TEST_SKIP_COMPARE AND NOT TEST_REFERENCE)
+endif ()
 
-#if (NOT TEST_ERRREF)
-#  set (ERROR_APPEND 1)
-#endif (NOT TEST_ERRREF)
+if (EXISTS ${TEST_FOLDER}/${TEST_OUTPUT})
+  file (REMOVE ${TEST_FOLDER}/${TEST_OUTPUT})
+endif ()
+
+if (EXISTS ${TEST_FOLDER}/${TEST_OUTPUT}.err)
+  file (REMOVE ${TEST_FOLDER}/${TEST_OUTPUT}.err)
+endif ()
 
 message (STATUS "COMMAND: ${TEST_PROGRAM} ${TEST_ARGS}")
+
+if (TEST_ENV_VAR)
+  set (ENV{${TEST_ENV_VAR}} "${TEST_ENV_VALUE}")
+endif ()
 
 if (NOT TEST_INPUT)
   # run the test program, capture the stdout/stderr and the result var
@@ -42,7 +50,7 @@ if (NOT TEST_INPUT)
       OUTPUT_VARIABLE TEST_OUT
       ERROR_VARIABLE TEST_ERROR
   )
-else (NOT TEST_INPUT)
+else ()
   # run the test program with stdin, capture the stdout/stderr and the result var
   execute_process (
       COMMAND ${TEST_PROGRAM} ${TEST_ARGS}
@@ -54,54 +62,91 @@ else (NOT TEST_INPUT)
       OUTPUT_VARIABLE TEST_OUT
       ERROR_VARIABLE TEST_ERROR
   )
-endif (NOT TEST_INPUT)
+endif ()
 
 message (STATUS "COMMAND Result: ${TEST_RESULT}")
 
-#if (ERROR_APPEND)
-#  file (READ ${TEST_FOLDER}/${TEST_OUTPUT}.err TEST_STREAM)
-#  file (APPEND ${TEST_FOLDER}/${TEST_OUTPUT} "${TEST_STREAM}") 
-#endif (ERROR_APPEND)
+# if the .err file exists and ERRROR_APPEND is enabled
+if (ERROR_APPEND AND EXISTS ${TEST_FOLDER}/${TEST_OUTPUT}.err)
+  file (READ ${TEST_FOLDER}/${TEST_OUTPUT}.err TEST_STREAM)
+  file (APPEND ${TEST_FOLDER}/${TEST_OUTPUT} "${TEST_STREAM}")
+endif ()
 
+# append the test result status with a predefined text
 if (TEST_APPEND)
   file (APPEND ${TEST_FOLDER}/${TEST_OUTPUT} "${TEST_APPEND} ${TEST_RESULT}\n")
-endif (TEST_APPEND)
+endif ()
 
 # if the return value is !=${TEST_EXPECT} bail out
 if (NOT ${TEST_RESULT} STREQUAL ${TEST_EXPECT})
   message ( FATAL_ERROR "Failed: Test program ${TEST_PROGRAM} exited != ${TEST_EXPECT}.\n${TEST_ERROR}")
-endif (NOT ${TEST_RESULT} STREQUAL ${TEST_EXPECT})
+endif ()
 
 message (STATUS "COMMAND Error: ${TEST_ERROR}")
 
+# if the output file needs Storage text removed
 if (TEST_MASK)
   file (READ ${TEST_FOLDER}/${TEST_OUTPUT} TEST_STREAM)
-   string (REGEX REPLACE "Storage:[^\n]+\n" "Storage:   <details removed for portability>\n" TEST_STREAM "${TEST_STREAM}")
+  string (REGEX REPLACE "Storage:[^\n]+\n" "Storage:   <details removed for portability>\n" TEST_STREAM "${TEST_STREAM}")
   file (WRITE ${TEST_FOLDER}/${TEST_OUTPUT} "${TEST_STREAM}")
-endif (TEST_MASK)
+endif ()
 
+# remove text from the output file
 if (TEST_FILTER)
   file (READ ${TEST_FOLDER}/${TEST_OUTPUT} TEST_STREAM)
-   string (REGEX REPLACE "${TEST_FILTER}" "" TEST_STREAM "${TEST_STREAM}")
+  string (REGEX REPLACE "${TEST_FILTER}" "" TEST_STREAM "${TEST_STREAM}")
   file (WRITE ${TEST_FOLDER}/${TEST_OUTPUT} "${TEST_STREAM}")
-endif (TEST_FILTER)
+endif ()
 
-if (WIN32 AND NOT MINGW)
-  file (READ ${TEST_FOLDER}/${TEST_REFERENCE} TEST_STREAM)
-  file (WRITE ${TEST_FOLDER}/${TEST_REFERENCE} "${TEST_STREAM}")
-endif (WIN32 AND NOT MINGW)
+# compare output files to references unless this must be skipped
+if (NOT TEST_SKIP_COMPARE)
+  if (WIN32 AND NOT MINGW)
+    file (READ ${TEST_FOLDER}/${TEST_REFERENCE} TEST_STREAM)
+    file (WRITE ${TEST_FOLDER}/${TEST_REFERENCE} "${TEST_STREAM}")
+  endif ()
 
-# now compare the output with the reference
-execute_process (
-    COMMAND ${CMAKE_COMMAND} -E compare_files ${TEST_FOLDER}/${TEST_OUTPUT} ${TEST_FOLDER}/${TEST_REFERENCE}
-    RESULT_VARIABLE TEST_RESULT
-)
+  # now compare the output with the reference
+  execute_process (
+      COMMAND ${CMAKE_COMMAND} -E compare_files ${TEST_FOLDER}/${TEST_OUTPUT} ${TEST_FOLDER}/${TEST_REFERENCE}
+      RESULT_VARIABLE TEST_RESULT
+  )
+  if (NOT ${TEST_RESULT} STREQUAL 0)
+    set (TEST_RESULT 0)
+    file (STRINGS ${TEST_FOLDER}/${TEST_OUTPUT} test_act)
+    list (LENGTH test_act len_act)
+    file (STRINGS ${TEST_FOLDER}/${TEST_REFERENCE} test_ref)
+    list (LENGTH test_ref len_ref)
+    if (NOT ${len_act} STREQUAL "0" AND NOT ${len_ref} STREQUAL "0")
+      math (EXPR _FP_LEN "${len_ref} - 1")
+      foreach (line RANGE 0 ${_FP_LEN})
+        list (GET test_act ${line} str_act)
+        list (GET test_ref ${line} str_ref)
+        if (NOT "${str_act}" STREQUAL "${str_ref}")
+          if (NOT "${str_act}" STREQUAL "")
+            set (TEST_RESULT 1)
+            message ("line = ${line}\n***ACTUAL: ${str_act}\n****REFER: ${str_ref}\n")
+          endif ()
+        endif ()
+      endforeach ()
+    else ()
+      if (${len_act} STREQUAL "0")
+        message (STATUS "COMPARE Failed: ${TEST_FOLDER}/${TEST_OUTPUT} is empty")
+      endif ()
+      if (${len_ref} STREQUAL "0")
+        message (STATUS "COMPARE Failed: ${TEST_FOLDER}/${TEST_REFERENCE} is empty")
+      endif ()
+    endif ()
+    if (NOT ${len_act} STREQUAL ${len_ref})
+      set (TEST_RESULT 1)
+    endif ()
+  endif ()
 
-# again, if return value is !=0 scream and shout
-if (TEST_RESULT)
-  message (FATAL_ERROR "Failed: The output of ${TEST_PROGRAM} did not match ${TEST_REFERENCE}")
-endif (TEST_RESULT)
+  message (STATUS "COMPARE Result: ${TEST_RESULT}")
+
+  # again, if return value is !=0 scream and shout
+  if (NOT ${TEST_RESULT} STREQUAL 0)
+    message (FATAL_ERROR "Failed: The output of ${TEST_OUTPUT} did not match ${TEST_REFERENCE}")
+  endif ()
 
 # everything went fine...
 message ("Passed: The output of ${TEST_PROGRAM} matches ${TEST_REFERENCE}")
-
