184b53b7d98793dd048e5ae1d162e9f1b4837874
Allen Byrne <byrn@hdfgroup.org>
2019-01-24 13:43:55 -0600

    CMake updates

--- config/cmake/HDF4UseFortran.cmake
+++ config/cmake/HDF4UseFortran.cmake
@@ -3,6 +3,15 @@
 #
 #-------------------------------------------------------------------------------
 enable_language (Fortran)
+set (HDF_PREFIX "H4")
+
+#-------------------------------------------------------------------------------
+#  Fix Fortran flags if we are compiling staticly on Windows using
+#  Windows_MT.cmake from config/cmake/UserMacros
+#-------------------------------------------------------------------------------
+if (BUILD_STATIC_CRT_LIBS)
+  TARGET_STATIC_CRT_FLAGS ()
+endif ()
 
 #-----------------------------------------------------------------------------
 # Detect name mangling convention used between Fortran and C
@@ -29,9 +38,9 @@ set (H4_F77_FUNC_ "H4_F77_FUNC_(name,NAME) ${CMAKE_MATCH_1}")
 #-----------------------------------------------------------------------------
 macro (CHECK_FORTRAN_FEATURE FUNCTION CODE VARIABLE)
     message (STATUS "Testing Fortran ${FUNCTION}")
-    if (CMAKE_REQUIRED_LIBRARIES)
+    if (HDF4_REQUIRED_LIBRARIES)
       set (CHECK_FUNCTION_EXISTS_ADD_LIBRARIES
-          "-DLINK_LIBRARIES:STRING=${CMAKE_REQUIRED_LIBRARIES}")
+          "-DLINK_LIBRARIES:STRING=${HDF4_REQUIRED_LIBRARIES}")
     else ()
       set (CHECK_FUNCTION_EXISTS_ADD_LIBRARIES)
     endif ()
@@ -64,7 +73,6 @@ macro (CHECK_FORTRAN_FEATURE FUNCTION CODE VARIABLE)
           "Determining if the Fortran ${FUNCTION} exists failed with the following output:\n"
           "${OUTPUT}\n\n")
     endif ()
-
 endmacro ()
 
 #-----------------------------------------------------------------------------
@@ -76,18 +84,18 @@ endmacro ()
 #-----------------------------------------------------------------------------
 
 # Check for Non-standard extension intrinsic function SIZEOF
-set(FORTRAN_HAVE_SIZEOF FALSE)
+set (${HDF_PREFIX}_FORTRAN_HAVE_SIZEOF FALSE)
 CHECK_FORTRAN_FEATURE(sizeof
   "
        PROGRAM main
        i = sizeof(x)
        END PROGRAM
   "
-  FORTRAN_HAVE_SIZEOF
+  ${HDF_PREFIX}_FORTRAN_HAVE_SIZEOF
 )
 
 # Check for F2008 standard intrinsic function C_SIZEOF
-set(FORTRAN_HAVE_C_SIZEOF FALSE)
+set (${HDF_PREFIX}_FORTRAN_HAVE_C_SIZEOF FALSE)
 CHECK_FORTRAN_FEATURE(c_sizeof
   "
        PROGRAM main
@@ -97,7 +105,7 @@ CHECK_FORTRAN_FEATURE(c_sizeof
          result = c_sizeof(a)
        END PROGRAM
   "
-  FORTRAN_HAVE_C_SIZEOF
+  ${HDF_PREFIX}_FORTRAN_HAVE_C_SIZEOF
 )
 
 # Check for F2008 standard intrinsic function STORAGE_SIZE
@@ -109,21 +117,21 @@ CHECK_FORTRAN_FEATURE(storage_size
          result = storage_size(a)
        END PROGRAM
   "
-  FORTRAN_HAVE_STORAGE_SIZE
+  ${HDF_PREFIX}_FORTRAN_HAVE_STORAGE_SIZE
 )
 
 # Check for F2008 standard intrinsic module "ISO_FORTRAN_ENV"
-set(HAVE_ISO_FORTRAN_ENV FALSE)
+set (${HDF_PREFIX}_HAVE_ISO_FORTRAN_ENV FALSE)
 CHECK_FORTRAN_FEATURE(ISO_FORTRAN_ENV
   "
        PROGRAM main
          USE, INTRINSIC :: ISO_FORTRAN_ENV
        END PROGRAM
   "
-  HAVE_ISO_FORTRAN_ENV
+  ${HDF_PREFIX}_HAVE_ISO_FORTRAN_ENV
 )
 
-set(FORTRAN_DEFAULT_REAL_NOT_DOUBLE FALSE)
+set (${HDF_PREFIX}_FORTRAN_DEFAULT_REAL_NOT_DOUBLE FALSE)
 CHECK_FORTRAN_FEATURE(RealIsNotDouble
   "
        MODULE type_mod
@@ -147,13 +155,13 @@ CHECK_FORTRAN_FEATURE(RealIsNotDouble
          CALL h4t(d)
        END PROGRAM main
   "
-  FORTRAN_DEFAULT_REAL_NOT_DOUBLE
+  ${HDF_PREFIX}_FORTRAN_DEFAULT_REAL_NOT_DOUBLE
 )
 
 #-----------------------------------------------------------------------------
 # Checks if the ISO_C_BINDING module meets all the requirements
 #-----------------------------------------------------------------------------
-set(FORTRAN_HAVE_ISO_C_BINDING FALSE)
+set (${HDF_PREFIX}_FORTRAN_HAVE_ISO_C_BINDING FALSE)
 CHECK_FORTRAN_FEATURE(iso_c_binding
   "
        PROGRAM main
@@ -166,7 +174,7 @@ CHECK_FORTRAN_FEATURE(iso_c_binding
             ptr = C_LOC(ichr(1:1))
        END PROGRAM
   "
-  FORTRAN_HAVE_ISO_C_BINDING
+  ${HDF_PREFIX}_FORTRAN_HAVE_ISO_C_BINDING
 )
 
 #-----------------------------------------------------------------------------
--- config/cmake/cacheinit.cmake
+++ config/cmake/cacheinit.cmake
@@ -20,4 +20,4 @@ set (HDF4_PACKAGE_NAME "hdf4" CACHE STRING "Name of HDF4 package" FORCE)
 
 set (PACK_EXAMPLE_SOURCE ON CACHE BOOL "Package the source" FORCE)
 
-set (CPACK_PACKAGE_VERSION "0.3.7" CACHE STRING "Version of HDF4 source package" FORCE)
+set (CPACK_PACKAGE_VERSION "0.3.8" CACHE STRING "Version of HDF4 source package" FORCE)
