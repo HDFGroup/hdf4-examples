1207d9db11437b7ddaf3cd79bdbe37a885cba6e6
Allen Byrne <byrn@hdfgroup.org>
2023-02-02 07:54:18 -0600

    Fix Intel compiler names

--- config/toolchain/intel.cmake
+++ config/toolchain/intel.cmake
@@ -3,23 +3,11 @@
 
 set (CMAKE_COMPILER_VENDOR "intel")
 
-<<<<<<< HEAD
-if (USE_SANITIZER)
-  set(CMAKE_C_COMPILER icl)
-  set(CMAKE_CXX_COMPILER icl++)
-  set(CMAKE_Fortran_COMPILER ifort)
-  set(INTEL_CLANG ON)
-elseif (USE_INTELX)
+if (USE_LLVM)
   set(CMAKE_C_COMPILER icx)
   set(CMAKE_CXX_COMPILER icpx)
   set(CMAKE_Fortran_COMPILER ifx)
-=======
-if(USE_SANITIZER)
-  set(CMAKE_C_COMPILER icl)
-  set(CMAKE_CXX_COMPILER icl++)
-  set(CMAKE_Fortran_COMPILER ifort)
   set(INTEL_CLANG ON)
->>>>>>> branch 'master' of https://github.com/HDFGroup/hdf4-examples.git
 else ()
   set(CMAKE_C_COMPILER icc)
   set(CMAKE_CXX_COMPILER icpc)
